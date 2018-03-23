72633d88e2f6964a03c0a20ba18ee7101a548e62
Boguslaw Krawczuk <boguslaw@oc8227423143.ibm.com>
2018-03-23 20:37:49 +0100

    one negative example for bool flag added

--- src/args.cpp
+++ src/args.cpp
@@ -9,8 +9,11 @@ using namespace std;
 
 Args::Args(const string in_schema, const int argc, const string argv[])
 {
+  flags["-b"] = false;
   if (argc > 1 and argv[1] != "")
+  {    
     flags[argv[1]] = true;
+  }
   else 
     schema = in_schema;
 }
